9e316b8aea6248fa003c8fd900daebd07bd80a3f
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-15 21:31:21 +0300

    c-n/form page

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,9 +1,61 @@
 <template>
   <v-container>
     <v-layout row>
-          <v-flex xs12>
-              <h1>NewAd</h1>
-          </v-flex>
+        <v-flex xs12 sm6 offset-sm3>
+          <h1 class="text--secondary mb-3">Create new ad</h1>
+          <v-form v-model="valid" ref="form" validation class="mb-3">
+            <v-text-field 
+              name="title" 
+              label="Ad title" 
+              type="text"
+              required
+              :rules="[v => !!v || 'Title is required']"
+              v-model="title"
+            ></v-text-field>
+            <v-textarea  
+              multi-line
+              name="discription" 
+              label="Ad discription" 
+              type="text"
+              v-model="description"
+              :rules="[v => !!v || 'Discription is required']"
+            ></v-textarea>
+          </v-form>
+          <v-layout row class="mb-3">
+            <v-flex xs12>
+              <v-btn
+                color="blue-grey"
+                class="white--text"
+              >
+                Upload
+              <v-icon right dark>cloud_upload</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="ml-3">
+            <v-flex xs12>
+              <img src="" height="100"/>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="ml-3">
+            <v-flex xs12>
+              <v-switch
+                label="Add to promo?"
+                v-model="promo"
+              ></v-switch>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="ml-3">
+            <v-flex xs12>
+              <v-spacer></v-spacer>
+              <v-btn 
+              class="success" 
+              @click="createAd"
+              :disabled="!valid"
+              >CreateAd</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-flex>
       </v-layout> 
   </v-container>
 </template>
@@ -11,7 +63,25 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        title: '',
+        description: '',
+        promo: false,
+        valid: false
+
+      }
+    },
+    methods: {
+      createAd () {
+        if (this.$refs.form.validate) {
+          const ad = {
+            title: this.title,
+            description: this.description,
+            promo: this.promo
+          }
+          console.log(ad)
+        }
+      }
     }
   }
 </script>
