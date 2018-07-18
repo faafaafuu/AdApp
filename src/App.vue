7333b3620872ed1e0614747c125ade808abb7fdd
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-18 18:06:49 +0300

    error output

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,24 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+    <template v-if="error">
+        <v-snackbar
+          color="error"
+          @input="closeError"
+          :multi-line="mode === 'multi-line'"
+          :timeout="5000"
+          :value="true"
+        >
+          {{error}}
+        <v-btn
+          color="white"
+          flat
+          @click="closeError"
+        >
+        &#10006;
+      </v-btn>
+    </v-snackbar>
+    </template>
   </v-app>
 </template>
 
@@ -62,6 +80,16 @@ export default {
         {title: 'New ad', icon: 'note_add', url: '/new'},
         {title: 'My ads', icon: 'list', url: '/list'}
       ]
+    }
+  },
+  methods: {
+    closeError () {
+      return this.$store.dispatch('clearError')
+    }
+  },
+  computed: {
+    error () {
+      return this.$store.getters.error
     }
   }
 }
